62295e3f3fbcb475b095e1ee4ad76a9204eb5467
Dominik Guzei <dominik_guzei@gmx.at>
2010-10-20 16:54:21 +0200

    better packaging, proper use of websockets, refactored class names

--- server/start.rb
+++ server/start.rb
@@ -1,24 +1,23 @@
 require 'rubygems'
 require 'em-websocket'
 
-require 'WebsocketHost'
-require 'WebsocketClient'
-require 'HostFactory'
+require 'Host'
+require 'Client'
+require 'HostManager'
 
 EventMachine.run {
 
-  EventMachine::WebSocket.start(:host => "0.0.0.0", :port => 10_000, :debug => true) do |ws|
+  EventMachine::WebSocket.start(:host => "0.0.0.0", :port => 10_000, :debug => true) do |websocket|
     
     # handle connection requests
-    ws.onopen {
+    websocket.onopen {
       # analyse request path to differentiate between clients and games
-      game = ws.request["Path"].match /game/
+      game = websocket.request["Path"].match /game/
       if(game)
-        game = EventMachine::HostFactory.add_host(ws)
+        game = EventMachine::Websocket::HostManager.add_host(websocket)
         
         # HOST MESSAGES
-        
-        game.onmessage do |msg|
+        game.websocket.onmessage do |msg|
           # message to all clients
           if(all = msg.match /all (.*)/)
             message = all[1] # extracted message
@@ -30,34 +29,35 @@ EventMachine.run {
             message = client[2]
             begin
               game.to_client(client_id, "game #{message}")
-            rescue EventMachine::ClientNotFoundException => exception
-              game.send("exception:clientNotFound #{client_id}")
+            rescue EventMachine::Websocket::ClientNotFoundException => exception
+              game.websocket.send("exception:clientNotFound #{client_id}")
             end # rescue
           end
         end # block
         
-        game.onclose do
-          EventMachine::HostFactory.remove_host(game.id)
+        game.websocket.onclose do
+          EventMachine::Websocket::HostManager.remove_host(game.id)
         end
         
-        game.send(game.id)
+        game.websocket.send(game.id)
         
       else
         # connecting route - /game_id/connect 
-        connect = ws.request["Path"].match /(\d*)\/connect\/([a-zA-Z0-9 ]*)/
+        connect = websocket.request["Path"].match /(\d*)\/connect\/([a-zA-Z0-9 ]*)/
+        
         if(connect)
           game_id = connect[1]
           deviceType = connect[2]
-          game = EventMachine::HostFactory.get_host(game_id)
-          client = game.add_client(ws, deviceType)
+          game = EventMachine::Websocket::HostManager.get_host(game_id)
+          client = game.add_client(websocket, deviceType)
           
           # CLIENT MESSAGES
           
-          client.onmessage do |msg|
+          client.websocket.onmessage do |msg|
             # message to host
             if(host = msg.match /game (.*)/)
               message = host[1] # extracted message
-              game.to_host("#{client.id} #{message}")
+              game.websocket.send("#{client.id} #{message}")
 
             # message to all clients
             elsif(all = msg.match /all (.*)/)
@@ -70,22 +70,22 @@ EventMachine.run {
                 message = msg_to_client[2]
                 begin
                   game.to_client(other_client_id, "#{client.id} #{message}")
-                rescue EventMachine::ClientNotFoundException => exception
-                  client.send("exception:clientNotFound #{client.id}")
+                rescue EventMachine::Websocket::ClientNotFoundException => exception
+                  client.websocket.send("exception:clientNotFound #{client.id}")
                 end # rescue
             end # if (host)
           end # client.onmessage
           
-          client.onclose do
-            client.send("game closed")
-            game.send("#{client.id} disconnected")
+          client.websocket.onclose do
+            client.websocket.send("game closed")
+            game.websocket.send("#{client.id} disconnected")
           end
           
-          game.send("#{client.id} connected")
-          client.send(client.id)
+          game.websocket.send("#{client.id} connected")
+          client.websocket.send(client.id)
           
         else
-          ws.close_with_error("connect to game like this: /*i/connect")
+          websocket.close_with_error("connect to game like this: /*i/connect")
         end
       end
     }
